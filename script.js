8f256e7b15ca96e17283f686ad7fbababfe5a5c4
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-11-16 16:40:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const score0El = document.querySelector("#score--0");
-const score1El = document.querySelector("#score--1");
-const current0El = document.querySelector("#current--0");
-const current1El = document.querySelector("#current--1");
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
 const diceEl = document.querySelector(".dice");
@@ -22,17 +22,15 @@ const initGame = () => {
   current1El.textContent = 0;
   score0El.textContent = 0;
   score1El.textContent = 0;
-  player0El.classList.remove("player--winner");
-  player1El.classList.remove("player--winner");
-  player1El.classList.remove("player--active");
-  player1El.classList.remove("player--active");
+  player0El.classList.remove("player--winner", "player--active");
+  player1El.classList.remove("player--winner", "player--active");
   player0El.classList.add("player--active");
   diceEl.classList.add("hidden");
 };
 
 initGame();
 
-const switchActivePlayer = () => {
+const togglePlayer = () => {
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent =
     currentScore;
@@ -43,36 +41,27 @@ const switchActivePlayer = () => {
 
 btnRoll.addEventListener("click", () => {
   if (isPlaying) {
-    // 1. Generede a random number
-
-    const diceNumber = Math.trunc(Math.random() * 6) + 1;
-
-    // 2. Display number on the dice
+    const diceValue = Math.trunc(Math.random() * 6) + 1;
 
     diceEl.classList.remove("hidden");
-    diceEl.src = `img/dice${diceNumber}.png`;
+    diceEl.src = `img/dice${diceValue}.png`;
 
-    // 3. If the number is 1, switch to the next player, if not - add number to the current score
-
-    if (diceNumber !== 1) {
-      currentScore += diceNumber;
+    if (diceValue !== 1) {
+      currentScore += diceValue;
       document.getElementById(`current--${activePlayer}`).textContent =
         currentScore;
     } else {
-      switchActivePlayer();
+      togglePlayer();
     }
   }
 });
 
 btnHold.addEventListener("click", () => {
   if (isPlaying) {
-    // 1. Add current score to active player total score
-
     totalScore[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       totalScore[activePlayer];
 
-    // 2. If total score of active player >= 100 , active player won , if not switch active player
     if (totalScore[activePlayer] >= 100) {
       isPlaying = false;
       document
@@ -83,7 +72,7 @@ btnHold.addEventListener("click", () => {
         .classList.remove("player--active");
       diceEl.classList.add("hidden");
     } else {
-      switchActivePlayer();
+      togglePlayer();
     }
   }
 });
